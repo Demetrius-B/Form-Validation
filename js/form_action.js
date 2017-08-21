83062d2776d5e2e7061899a825fdbb10d18f9a46
Demetrius Berkeley <Dberkeley@fullsail.edu>
2017-08-20 20:14:14 -0400

    Working on js. Need to add customValidity for each input

--- js/form_action.js
+++ js/form_action.js
@@ -1,13 +1,37 @@
-const submit = document.querySelector('.submitBtn');
+const submit = document.querySelector('button');
 const grBtn = document.querySelector('#grBtn');
 const sBtn = document.querySelector('#sBtn');
 const nameField = document.querySelector('#fullName1');
 
+class CheckValidity {
+  constructor(input, type) {
+    this.input = input;
+    this.type = type;
+    this.errors = [];
+  }
+  addError(message) {
+    this.errors.push(message);
+  }
+  getMessage() {
+    const status = this.input.validity
+
+    if (status.typeMismatch) {
+      this.addError('Invalid Entry');
+    }
+    if (status.tooLong) {
+      this.addError('Entry is too long')
+    }
+    if (!this.input.value.match(/[A-Z]/g)) {
+      this.addError('Entry must contain at least one uppercase')
+    }
+    return this.errors
+  }
+}
+
 
 submit.addEventListener("click", (event) => {
-  if (nameField.validity.typeMismatch) {
-    nameField.setCustomValidity("ERRORR")
-  } else {
-    nameField.setCustomValidity("")
-  }
-})
+  event.preventDefault();
+  let validateName = new CheckValidity(nameField, "fullname");
+  let errorMessages = validateName.getMessage();
+  console.log(errorMessages);
+});
